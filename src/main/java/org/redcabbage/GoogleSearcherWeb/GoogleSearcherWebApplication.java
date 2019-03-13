d4c7afa132aa027aa49456e5a09d8bb838f1d58a
Simon Woodward <simon@redcabbage.org>
2019-03-13 22:47:33 +0000

    First checkin from initializr

---
+++ src/main/java/org/redcabbage/GoogleSearcherWeb/GoogleSearcherWebApplication.java
@@ -0,0 +1,13 @@
+package org.redcabbage.GoogleSearcherWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSearcherWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSearcherWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/redcabbage/GoogleSearcherWeb/GoogleSearcherWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.redcabbage.GoogleSearcherWeb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleSearcherWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
