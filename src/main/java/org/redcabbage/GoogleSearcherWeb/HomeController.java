be68fa58a9ec4618d2f8b126816904f78a0e8fd6
Simon Woodward <simon@redcabbage.org>
2019-03-17 18:17:36 +0000

    First implementation of the searcher with a hardcoded set of search results.  Still need to write a test case for the controller with the changes.

--- src/main/java/org/redcabbage/GoogleSearcherWeb/HomeController.java
+++ src/main/java/org/redcabbage/GoogleSearcherWeb/HomeController.java
@@ -1,22 +1,39 @@
 package org.redcabbage.GoogleSearcherWeb;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.redcabbage.GoogleSearcherWeb.model.SearchResult;
+import org.redcabbage.GoogleSearcherWeb.model.Searcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.logging.Logger;
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+@Log4j2
 public class HomeController {
-  @GetMapping("/")
-  public String home(Model model) {
+  private static final String SEARCH_RESULTS = "searchResults";
+
+  private final Searcher searcher;
+
+  @GetMapping()
+  public String get(@RequestParam(name = "search", required = false) String search,
+                    Model model) {
+//TODO: Add a test case for this controller.
+    // Perform the search, if we need to
+    if (search != null) {
+      List<SearchResult> searchRes = searcher.search(search);
+      log.error("Search results count: " + searchRes.size());
+      model.addAttribute(SEARCH_RESULTS, searchRes);
+    }
+
     return "home";
   }
 
-  @PostMapping("/")
-  public String postHome() {
-    return "home";
-  }
 }
 
